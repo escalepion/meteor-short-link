06cdaf1b8c4582a765a999cb8691178471401bd5
escalepion <escalepion@gmail.com>
2017-09-25 15:38:55 +0300

    add visited time

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import Clipboard from 'clipboard';
+import moment from 'moment';
 
 export default class LinksListItem extends Component {
     constructor(props) {
@@ -25,13 +26,23 @@ export default class LinksListItem extends Component {
     componentWillUnmount() {
         this.clipboard.destroy();
     }
+    renderStats () {
+        const visitMessage = this.props.visitedCount === 1 ? 'visit' : 'visits';
+        let visitedMessage = null;
+
+        if(typeof this.props.lastVisitedAt === 'number') {
+            let lastTimeMoment = moment(this.props.lastVisitedAt);
+            visitedMessage = `last visited ${lastTimeMoment.format('MMM Do, YYYY hh:mm a')} (${lastTimeMoment.fromNow()})`;
+        }
+        return <p>{this.props.visitedCount} {visitMessage} {visitedMessage}</p>
+    }
     render() {
         return (
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
                 <p>{this.props.visible.toString()}</p>
-                <p>{this.props.visitedCount} - {this.props.lastVisitedAt}</p>
+                {this.renderStats()}
                 <button ref="copy" data-clipboard-text={this.props.shortUrl}>
                 {this.state.justCopied ? 'Copied' : 'Copy'}
                 </button>
