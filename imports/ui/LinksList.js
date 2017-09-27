93442ef60ad69183449a1d47b0355f9ea9a12f00
escalepion <escalepion@gmail.com>
2017-09-27 13:19:03 +0300

    add no link found message

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -26,6 +26,14 @@ export default class LinksList extends Component {
         this.linksTracker.stop();
     }
     renderLinksListItems() {
+        if (this.state.links.length === 0) {
+           return (
+               <div>
+                <p>No links found</p>
+               </div>
+           ); 
+        }
+
         return this.state.links.map((link) => {
             const shortUrl = Meteor.absoluteUrl(link._id);
             return <LinksListItem key={link._id} shortUrl={shortUrl} {...link} />
@@ -35,7 +43,6 @@ export default class LinksList extends Component {
     render () {
         return (
             <div>
-                <p>Links List</p>
                 <div>
                     {this.renderLinksListItems()}
                 </div>
